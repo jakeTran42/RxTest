bb6bf32cedf52dba532b031c99b23f0874e5c726
jakeTran42 <jake.tranehs@gmail.com>
2018-04-13 13:43:51 -0700

    add register selection

--- controlers/index.js
+++ controlers/index.js
@@ -1,10 +1,19 @@
 module.exports = (app) => {
-  app.get('/', function(req, res) {
+    app.get('/', function(req, res) {
        res.render('index');
-   });
+    });
 
-   app.get('/pharmacist', function(req, res) {
+    app.get('/pharmacist', function(req, res) {
         res.render('pharmacist-portal');
     });
+
+    app.get('/doctor', function(req, res) {
+        res.render('doctor-portal');
+    });
+
+    app.get('/reg-selection', function(req, res) {
+         res.render('reg-selection');
+     });
+
 
 }
